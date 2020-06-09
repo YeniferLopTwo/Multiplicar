5a9552a8e5e453a5399bc8e40bb8b26d21fa372c
YENIFER LOPEZ <YENIFER LOPEZ@DESKTOP-DEBM9B9>
2020-06-09 19:09:22 +0100

    Commit inicial

--- src/tablasmultiplicar/TablasMultiplicar.java
+++ src/tablasmultiplicar/TablasMultiplicar.java
@@ -1,27 +1,18 @@
 package tablasmultiplicar;
 
 import java.util.Scanner;
-/**
- * 
- * @author YENIFER LOPEZ
- */
+
 public class TablasMultiplicar {
-/**
- * 
- * @param num le pasa una variable número
- */
+
     public static void multiplicar(int num) {
-        for (int i = 1; i <= num; i++) { //necesitamos hacer una matriz
+        for (int i = 1; i <= num; i++) { 
             for (int j = 1; j <= 10; j++) {
                 System.out.println(i + " X " + j + " = " + i * j);
             }
             System.out.println("");
         }
     }
-/**
- * 
- * @param args comprueba de que no introduzcas una letra
- */
+
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         int num = 0;
