1f85ff218e65ee4a4c8f74b4de598312586b7e6e
YENIFER LOPEZ <YENIFER LOPEZ@DESKTOP-DEBM9B9>
2020-06-09 19:04:26 +0100

    Commit añadiendo comentarios

---
+++ src/tablasmultiplicar/TablasMultiplicar.java
@@ -0,0 +1,43 @@
+package tablasmultiplicar;
+
+import java.util.Scanner;
+/**
+ * 
+ * @author YENIFER LOPEZ
+ */
+public class TablasMultiplicar {
+/**
+ * 
+ * @param num 
+ */
+    public static void multiplicar(int num) {
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " X " + j + " = " + i * j);
+            }
+            System.out.println("");
+        }
+    }
+/**
+ * 
+ * @param args 
+ */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int num = 0;
+        boolean seguir = true;
+        while (seguir) {
+            try {
+                seguir = true;
+                System.out.println("¿Qué tabla quieres multiplicar?");
+                num = teclado.nextInt();
+                multiplicar(num); //procedimiento
+                seguir = false;
+            } catch (Exception letra) {
+                System.out.println("Solo números");
+                seguir = true;
+                teclado.next();
+            }
+        }
+    }
+}
